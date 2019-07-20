b621841cc00592a4cdd2a3f4ad17de4f35e779b1
kylee <lky1001@gmail.com>
2019-07-20 15:36:23 +0900

    test

--- app/src/main/java/com/tistory/lky1001/androidonvif/MainActivity.java
+++ app/src/main/java/com/tistory/lky1001/androidonvif/MainActivity.java
@@ -3,6 +3,8 @@ package com.tistory.lky1001.androidonvif;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tistory.lky1001.androidonvif.lib.OnvifManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        OnvifManager onvifManager = new OnvifManager();
     }
 }
